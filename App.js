9f1da92684793651ad2ed285cfe1e0684bc46876
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-12 18:51:37 -0400

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi, my name is Sutton, and I am a mobile developer".</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d0d0d0',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontWeight: "bold",
   },
 });
