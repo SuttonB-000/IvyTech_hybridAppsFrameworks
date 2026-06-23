7cc839205560c8f9b0835059e44609e8eb0a2f59
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-23 19:17:26 -0400

    Refactor index.js to define App component structure

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import { registerRootComponent } from 'expo';
-
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#3</Text>
+      </View>
+    </View>
+  )
+}
